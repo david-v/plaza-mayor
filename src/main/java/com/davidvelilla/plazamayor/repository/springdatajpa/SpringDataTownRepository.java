29970dfb8d9d23213c45400296e4eb2fad7fcc88
David Velilla <david@neonplay.com>
2016-02-23 17:01:44 +0000

    SpringDataJpa adapter up and running

--- src/main/java/com/davidvelilla/plazamayor/repository/springdatajpa/SpringDataTownRepository.java
+++ src/main/java/com/davidvelilla/plazamayor/repository/springdatajpa/SpringDataTownRepository.java
@@ -2,9 +2,19 @@ package com.davidvelilla.plazamayor.repository.springdatajpa;
 
 import com.davidvelilla.plazamayor.model.Town;
 import com.davidvelilla.plazamayor.repository.TownRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
 
 public interface SpringDataTownRepository extends TownRepository, Repository<Town, Integer>
 {
+    @Override
+    @Query("SELECT DISTINCT town FROM Town town left join fetch town.region WHERE town.name LIKE :name")
+    Collection<Town> findByName(@Param("name") String name);
 
+    @Override
+    @Query("SELECT town FROM Town town left join fetch town.region WHERE town.id =:id")
+    Town findById(@Param("id") int id);
 }
